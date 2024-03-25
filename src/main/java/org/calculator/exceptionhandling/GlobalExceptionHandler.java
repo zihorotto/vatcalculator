019d121d74430cbd94429a903a0673fee9505673
OttoZihor <zihorotto@gmail.com>
2024-03-25 10:24:23 +0100

    Global ExceptionHandler done

---
+++ src/main/java/org/calculator/exceptionhandling/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package org.calculator.exceptionhandling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValCalculationException.class)
+    public ResponseEntity<List<ValidationError>> handleVatCalculationException(ValCalculationException exception) {
+        ValidationError validationError = new ValidationError("VAT Calculation",
+                "Error occurred during VAT calculation: " + exception.getMessage());
+        log.error("Error in VAT calculation: " + exception.getMessage());
+        return new ResponseEntity<>(List.of(validationError), HttpStatus.BAD_REQUEST);
+    }
+}
