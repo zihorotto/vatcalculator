55f2b78ca2cf141f073a3e88c1a7a4a637fd65a0
OttoZihor <zihorotto@gmail.com>
2024-03-25 10:24:40 +0100

    Validation Error created

---
+++ src/main/java/org/calculator/exceptionhandling/ValidationError.java
@@ -0,0 +1,19 @@
+package org.calculator.exceptionhandling;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationError {
+
+
+    private String field;
+
+    private String errorMessage;
+}
